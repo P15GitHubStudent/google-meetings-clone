778dcb3603b00bf213c87bd3d71b2c08d505bf5a
Alexandros Toulakis <alextoulaki@gmail.com>
2023-04-27 11:48:35 +0100

    message chat tweaks

--- src/components/MeetingChat.vue
+++ src/components/MeetingChat.vue
@@ -1,107 +1,79 @@
 <template>
-  <q-btn-dropdown
-    class="btn-meeting-info"
-    :class="{ 'dropdown-visible-content': isDropdownContentVisible }"
-    @show="isDropdownContentVisible = true"
-    @hide="isDropdownContentVisible = false"
-    icon="info"
-  >
-    <q-card class="meeting-info-card move-left-right">
-      <q-card-section class="row items-center q-pb-none q-pl">
-        <div class="text-h6">Meeting Chat</div>
-        <q-space />
-        <q-btn
-          icon="close"
-          flat
-          round
-          dense
-          v-close-popup
-          @click="onCloseButton"
-        />
-      </q-card-section>
-
-      <q-card-section>
-        <!-- <q-chat-message :text="['hey, how are you?']" sent /> -->
-        <div class="chat-messages-container">
-          <div class="chat-message">
-            <div class="chat-message-header">
-              <span>Name</span>
-              <span>Alex</span>
-            </div>
-            <div class="chat-message-text">Here goes the message</div>
-          </div>
-
-          <div class="chat-message">
-            <div class="chat-message-header">
-              <span>Name</span>
-              <span>Alex</span>
-            </div>
-            <div class="chat-message-text">Here goes the message</div>
-          </div>
-        </div>
-      </q-card-section>
+  <q-card class="chat-meeting move-left-right">
+    <q-card-section class="row items-center q-pb-none q-pl">
+      <div class="text-h6">Chat Messages</div>
       <q-space />
-      <q-card-section class="chat-send-message-section">
-        <q-input filled v-model="text" label="Filled" />
-      </q-card-section>
-    </q-card>
-  </q-btn-dropdown>
+      <q-btn
+        icon="close"
+        flat
+        round
+        dense
+        v-close-popup
+        @click="onCloseButton"
+      />
+    </q-card-section>
+
+    <q-card-section class="q-pa-md column col justify-end">
+      <div class="chat-message">
+        <div>Alex - 10:30AM</div>
+        <p>This is a text</p>
+      </div>
+    </q-card-section>
+
+    <q-footer elevated class="bg-blue-grey-8 text-white">
+      <q-input></q-input>
+    </q-footer>
+  </q-card>
 </template>
 
 <script>
 export default {
   name: "meeting-chat",
-  props: {
-    value: {
-      type: Boolean,
-      default: false,
-    },
-    meetingUrl: {
-      type: String,
-    },
-  },
   data() {
     return {
-      isDropdownContentVisible: false,
-      text: "",
+      newMessage: "",
+      messages: [
+        {
+          text: "Hey Jim, how are you?",
+          from: "me",
+        },
+        {
+          text: "Good thanks, Danny! How are you?",
+          from: "them",
+        },
+        {
+          text: "Pretty good!",
+          from: "me",
+        },
+      ],
     };
   },
   methods: {
     onCloseButton() {
-      this.$emit("input", false);
+      this.$emit("close");
+    },
+    sendMessage() {
+      this.messages.push({
+        text: this.newMessage,
+        from: "me",
+      });
     },
   },
 };
 </script>
 
-<style scoped>
-.chat-messages-container {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-
-.btn-meeting-info {
-  border: none !important;
-}
-
-.chat-send-message-section {
-  margin-top: auto;
-  background-color: red;
-}
-
-.dropdown-visible-content {
-  color: rgb(138, 180, 248);
-}
-
-.meeting-info-card {
+<style>
+.chat-meeting {
   word-wrap: break-word;
   white-space: pre-wrap;
-  min-width: 20rem;
+  width: 100%;
   min-height: 30rem;
   background-color: #fff;
   color: black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow-x: hidden;
 }
 </style>
