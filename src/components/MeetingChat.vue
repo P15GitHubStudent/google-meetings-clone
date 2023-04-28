0349c40c891dc55b5eda44d5ac4761b8177ed604
Alexandros Toulakis <alextoulaki@gmail.com>
2023-04-28 13:52:57 +0100

    meeting chat input css tweaks

--- src/components/MeetingChat.vue
+++ src/components/MeetingChat.vue
@@ -1,3 +1,4 @@
+t
 <template>
   <q-card class="chat-meeting move-left-right">
     <q-card-section class="row items-center q-pb-none q-pl">
@@ -20,8 +21,28 @@
       </div>
     </q-card-section>
 
-    <q-footer elevated class="bg-blue-grey-8 text-white">
-      <q-input></q-input>
+    <q-footer class="bg-white">
+      <!-- <q-input rounded outlined v-model="text">
+        <template v-slot:append>
+          <q-icon
+            v-if="text !== ''"
+            name="schedule"
+            @click="text = ''"
+            class="cursor-pointer"
+          />
+          <q-icon name="schedule" />
+        </template>
+      </q-input> -->
+      <div class="flex position-relative">
+        <input type="text" class="input-chat-message" placeholder="message" />
+        <q-btn
+          icon="send"
+          class="send-chat-message-btn"
+          flat
+          rounded
+          text-color="blue"
+        ></q-btn>
+      </div>
     </q-footer>
   </q-card>
 </template>
@@ -31,7 +52,7 @@ export default {
   name: "meeting-chat",
   data() {
     return {
-      newMessage: "",
+      text: "",
       messages: [
         {
           text: "Hey Jim, how are you?",
@@ -63,6 +84,30 @@ export default {
 </script>
 
 <style>
+.send-chat-message-btn {
+  position: absolute;
+  top: 50%;
+  right: 0.2rem;
+  transform: translate(-50%, -50%);
+}
+
+.input-chat-message:focus {
+  outline-width: 0;
+}
+
+.input-chat-message {
+  background: rgb(241 243 244);
+  border-radius: 25px;
+  display: flex;
+  min-height: 2.25rem;
+  margin: 0.9375rem;
+  overflow: hidden;
+  position: relative;
+  width: 90%;
+  padding-right: 2rem;
+  padding-left: 2rem;
+}
+
 .chat-meeting {
   word-wrap: break-word;
   white-space: pre-wrap;
@@ -75,5 +120,7 @@ export default {
   left: 50%;
   transform: translate(-50%, -50%);
   overflow-x: hidden;
+  padding-right: 1rem;
+  padding-left: 1rem;
 }
 </style>
